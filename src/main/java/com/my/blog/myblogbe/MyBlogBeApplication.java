f999f356540141c59aba70928f38f776a28860ad
Pavel Samsonenko <pa.samsonenko@ssu.edu.ua>
2023-04-21 17:32:01 +0300

    initial commit

---
+++ src/main/java/com/my/blog/myblogbe/MyBlogBeApplication.java
@@ -0,0 +1,13 @@
+package com.my.blog.myblogbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBlogBeApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MyBlogBeApplication.class, args);
+  }
+
+}
